b677de8f022dee41f80d1c29699cdc4c95d458b2
Yuki Ban <ban@cyber.t.u-tokyo.ac.jp>
2014-09-27 14:12:30 +0900

    modified gameutil

--- src/clj/astrocats/gameutil.clj
+++ src/clj/astrocats/gameutil.clj
@@ -9,22 +9,3 @@
         is-hitfrom-top ]
     ))
 
-(defrecord Coin [id radius theta exist])
-
-(defn init-coin
-  [theta radius]
-  (map->Coin {:id (gensym) :radius radius
-              :theta theta :exist true}))
-
-(defrecord Block [id radius start end height])
-
-(defn init-block
-  [start end radius height]
-  (map->Block {:id (gensym) :radius radius
-               :start start :end end
-               :height height}))
-
-(defrecord Map [width height ground-y 
-                center-x center-y blocks])
-
-(defn init-map
