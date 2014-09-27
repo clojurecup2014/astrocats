8b53d095f20978edec97bf6d7d63c6a0dbd8c5ba
Ketaro Takeuchi <tiyotiyouda@gmail.com>
2014-09-27 09:36:50 +0900

    added initial ring-websocket codes

---
+++ astrocats/src/astrocats/echo.clj
@@ -0,0 +1,34 @@
+(ns astrocats.echo
+  (:require [ring-jetty.util.ws :as ws]))
+
+(def all-sessions (ref #{}))
+
+(defn- on-connect [session]
+  (dosync
+   (alter all-sessions conj session)))
+
+(defn- on-close [session code reason]
+  (dosync
+   (alter all-sessions disj session)))
+
+(defn- on-text [session message]
+  (doseq [s @all-sessions]
+    (ws/send! s (str
+                 (.. session getSession getRemoteAddress getHostName)
+                 ":"
+                 message))))
+
+(defn- on-bytes [session payload offset len]
+  nil)
+
+(defn- on-error [session e]
+  (.printStackTrace e)
+  (dosync
+   (alter all-sessions disj session)))
+
+(def handler
+  {:on-connect on-connect
+   :on-error on-error
+   :on-text on-text
+   :on-close on-close
+   :on-bytes on-bytes})
---
+++ astrocats/src/astrocats/handler.clj
@@ -0,0 +1,22 @@
+(ns astrocats.handler
+  (:require [compojure.core :refer :all]
+            [compojure.handler :as handler]
+            [compojure.route :as route]
+            [hiccup.core :refer :all]
+            [hiccup.page :refer :all]))
+
+(defn- page []
+  (html5 [:head [:title "astrocats"] (include-js "example.js")]
+         [:body
+          [:div#form
+           [:input#send-text {:type "text"}]
+           [:input#send-button {:type "button" :value "Send"}]]
+          [:div#message]]))
+
+(defroutes app-routes
+  (GET "/" [] (page))
+  (route/resources "/")
+  (route/not-found "Not Found"))
+
+(def app
+  (handler/site app-routes))
