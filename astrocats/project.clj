80ba0838813f4d0c2706d86e6d45e70190fc557e
Ketaro Takeuchi <tiyotiyouda@gmail.com>
2014-09-27 09:33:35 +0900

    init blank ring-websocket codes

--- astrocats/project.clj
+++ astrocats/project.clj
@@ -3,4 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.1.9"]
+                 [hiccup "1.0.5"]
+                 [ring-jetty/ring-ws "0.1.0-SNAPSHOT"]]
+  :plugins [[lein-ring-jetty "0.1.0-SNAPSHOT"]]
+  :ring {:handler astrocats.handler/app
+         :websockets {"/echo" astrocats.echo/handler}}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/javax.servlet-api "3.1.0"]
+                        [ring-mock "0.1.5"]]}})
