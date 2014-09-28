da08ef5cde1e83d7bb2ad0445830f9664252bcc5
atsuo yamada <yamada-git@tir.jp>
2014-09-29 00:25:54 +0900

    Connet WebSocket at press start button

--- src/cljs/astrocats/core.cljs
+++ src/cljs/astrocats/core.cljs
@@ -34,14 +34,16 @@
                      (reset! z-state :release)))
                  30))
 
-(defn ^:export main []
+(defn listen-start! []
     (reset! ws (socket/socket
                  (str (socket/get-host-url) "echo/")))
     ;; set open websocket handler
     (socket/listen! @ws :open (fn [e]))
     ;; set msg websocket handler
-    (socket/listen! @ws :msg (fn [e] (ac-view/call-event! (. e -data))))
+    (socket/listen! @ws :msg (fn [e] (ac-view/call-event! (. e -data)))))
+
+(defn ^:export main []
     ;; start view
-    (ac-view/bootstrap "game" l-listener r-listener z-listener))
+    (ac-view/bootstrap "game" l-listener r-listener z-listener listen-start!))
 
 (set! (.-onload js/window) main)
