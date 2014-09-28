7c8f0fd8d81c77551c7aca277cb38e0ebfbc4ba2
Ketaro Takeuchi <tiyotiyouda@gmail.com>
2014-09-28 13:16:48 +0900

    removed print

--- src/cljs/astrocats/core.cljs
+++ src/cljs/astrocats/core.cljs
@@ -2,8 +2,6 @@
   (:require [astrocats.socket :as socket]
             [ac-view.core :as ac-view]))
 
-(set! *print-fn* #(.log js/console (apply str %&))) ; TODO remove this
-
 (def game-map (atom nil))
 (def ws (atom nil))
 
@@ -26,10 +24,9 @@
     (reset! ws (socket/socket
                  (str (socket/get-host-url) "echo/")))
     ;; set open websocket handler
-    (socket/listen! @ws :open (fn [e] (print "connected!")))
+    (socket/listen! @ws :open (fn [e]))
     ;; set msg websocket handler
-    (socket/listen! @ws :msg (fn [e] (print "on message: " (. e -data))
-                                     (ac-view/call-event! (. e -data))))
+    (socket/listen! @ws :msg (fn [e] (ac-view/call-event! (. e -data))))
     ;; start view
     (ac-view/bootstrap "game" l-listener r-listener z-listener))
 
